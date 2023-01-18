b97db6c00d4b8543791b07dd8390a2256a35b8ce
unknown <amandahsieh.926188@gmail.com>
2023-01-19 04:32:11 +0800

    deleted

--- 215.cpp
+++
@@ -1,7 +0,0 @@
-class Solution {
-public:
-    int findKthLargest(vector<int>& nums, int k) {
-        nth_element(nums.begin(), nums.end()-k, nums.end());
-        return nums[nums.size()-k];
-    }
-};
