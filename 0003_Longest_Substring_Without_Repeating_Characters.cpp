de1c3b0b88a6ef96ee0a9ac213c52065255f82aa
Hsin-Yu Hsieh <amandahsieh.926188@gmail.com>
2023-02-14 01:59:48 +0800

    100_99.61

--- 0003_Longest_Substring_Without_Repeating_Characters.cpp
+++ 0003_Longest_Substring_Without_Repeating_Characters.cpp
@@ -1,24 +1,24 @@
 /*--------------------------Author: Hsin-Yu Hsieh--------------------------*/
-/*First Trial: Used -> new start, new substring   T:7 93.87%   M:6.9 92.25%*/
+/*First Trial: Used -> new start, new substring     T:0 100%   M:6.8 99.61%*/
 
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        if (s.size() == 0)  return 0;
+        ios_base::sync_with_stdio(false);
+        int l=s.size(), i, j;
+        if (!l)  return 0;
         int len = 1, start = 0;
-        int used[150] = {0};
-        for (int i=0 ; i<s.size() ; i++){
-            for (int j=start ; j<s.size() ; j++){
-                if (used[s[j]] == 1){
-                    len = max(len, j - i);
+        int used[128] = {0};
+        for (i=0 ; i<l ; i++){
+            for (j=start ; j<l ; j++){
+                if (used[s[j]]){
+                    len = (len>j-i)? len:j-i;
                     start = j;
                     used[s[i]] = 0;
                     break;
-                }    
-                else{
-                    used[s[j]] = 1;
-                    if (j == s.size()-1)    len = max(len, j-i+1);
                 }
+                used[s[j]] = 1;
+                if (j == l-1)    len = (len>j-i+1)? len:j-i+1;
             }
         }
         return len;
