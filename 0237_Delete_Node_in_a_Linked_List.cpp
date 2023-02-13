d96d744287cfddf31f18d486cde3b28c911581bb
Hsin-Yu Hsieh <amandahsieh.926188@gmail.com>
2023-02-14 01:57:03 +0800

    82.93_18.91

---
+++ 0237_Delete_Node_in_a_Linked_List.cpp
@@ -0,0 +1,19 @@
+/*----------------Author: Hsin-Yu Hsieh----------------*/
+/*First Trial: value & next   T:12 82.93%  M:7.7 18.91%*/
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        node->val = node->next->val;
+        node->next = node->next->next;
+    }
+};
