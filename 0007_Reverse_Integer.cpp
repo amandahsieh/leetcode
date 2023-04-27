9fc03e54b411dc14f12a1761c6e5a0adc92143b1
Hsin-Yu Hsieh <amandahsieh.926188@gmail.com>
2023-04-28 01:18:10 +0800

    023.53_060.40

---
+++ 0007_Reverse_Integer.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long long int ans = 0;
+        int l = pow(2,31)*-1;
+        int u = pow(2,31)-1;
+        /*Dealing with the negative problem*/
+        int neg = (x<0)? -1:1;
+        x = abs(x);
+        while (x>0){
+            ans = ans*10 + x%10;
+            if (ans>u || ans<l){
+                ans=0;
+                break;
+            }
+            x/=10;
+        }
+        return ans * neg;
+    }
+};
